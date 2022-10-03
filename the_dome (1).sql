a32a7e064022aed6631f2adac0f515f9de2b4881
Tariq Horan <N00202437@iadt.ie>
2022-10-03 13:50:32 +0100

    The Dome movies datbase

---
+++ the_dome (1).sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 15, 2021 at 01:44 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `the_dome`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `bookings_date` date DEFAULT NULL,
+  `location` enum('indoor','outdoor') DEFAULT 'indoor',
+  `hot_food` enum('Hot Dogs','Pizza','Wings','Nachos') DEFAULT 'Hot Dogs',
+  `snacks` enum('popcorn','candy floss','pick&mix','malteasers') DEFAULT 'popcorn',
+  `ticket_price` decimal(50,0) DEFAULT NULL,
+  `customer_id` int(10) UNSIGNED DEFAULT NULL,
+  `showing_id` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `memberships`
+--
+
+CREATE TABLE `memberships` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `email_address` varchar(50) DEFAULT NULL,
+  `customer_id` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movies`
+--
+
+CREATE TABLE `movies` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `runtime` time DEFAULT NULL,
+  `release_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `showings`
+--
+
+CREATE TABLE `showings` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `runtime` time DEFAULT NULL,
+  `showing_date` date DEFAULT NULL,
+  `genre` enum('Horror','Action','Comedy','Sci-fi','') DEFAULT NULL,
+  `movie_id` int(11) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_customers_movie` (`customer_id`),
+  ADD KEY `fk_shoiwngs_movie` (`showing_id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `memberships`
+--
+ALTER TABLE `memberships`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_memberships_customer` (`customer_id`);
+
+--
+-- Indexes for table `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `showings`
+--
+ALTER TABLE `showings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_showings_movie` (`movie_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `memberships`
+--
+ALTER TABLE `memberships`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `showings`
+--
+ALTER TABLE `showings`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `fk_customers_movie` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+  ADD CONSTRAINT `fk_shoiwngs_movie` FOREIGN KEY (`showing_id`) REFERENCES `showings` (`id`);
+
+--
+-- Constraints for table `memberships`
+--
+ALTER TABLE `memberships`
+  ADD CONSTRAINT `fk_memberships_customer` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`);
+
+--
+-- Constraints for table `showings`
+--
+ALTER TABLE `showings`
+  ADD CONSTRAINT `fk_showings_movie` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
